71f564880b4e3afec6b30a349bae23404b5b181b
Alma <almastar405@gmail.com>
2023-10-10 01:27:04 -0500

    initializing methods

---
+++ src/AACCategory.java
@@ -0,0 +1,34 @@
+public class AACCategory {
+    
+    /*
+     * constructors
+     */
+    public AACCategory (String name){
+        //stub
+    }//AACCategory
+
+    /*
+     * methods
+     */
+
+     public void addItem (String imageLoc, String text){
+        //stub
+     }//addItem
+
+     public String getCategory() {
+        return "getCategory";//stub
+     }//getCategory
+
+     public String getText (String imageLoc){
+        return "getText";//stub
+     }//getText
+
+     public boolean hasImage (String imageLoc){
+        return false;//stub
+     }//hasImage
+
+     public String[] getImages(){
+        return null;//stub
+     }//getImages
+
+}//class AACCategory
